2d01a63167843b839f65e07bfcab7bcfd6f1484b
Aleksandar Babic <alekski64@icloud.com>
2018-06-19 17:01:33 +0200

    Implemented github parser

--- parsers/github.js
+++ parsers/github.js
@@ -1,5 +1,18 @@
 class GithubParser {
   constructor() {}
+
+  parse(webhookPayload) {
+    const branch = webhookPayload.ref.split('/')[2];
+    return {
+      repositoryId: webhookPayload.repository.id,
+      repositoryName: webhookPayload.repository.name,
+      repositoryFullName: webhookPayload.repository.full_name,
+      repositoryUrl: webhookPayload.repository.url,
+      repositoryBranch: branch,
+      isPrivate: webhookPayload.repository.private,
+      updatedAt: webhookPayload.repository.updated_at
+    };
+  }
 }
 
 module.exports = GithubParser;
