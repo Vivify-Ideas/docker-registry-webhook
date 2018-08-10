5f9260d3ca013b6e03b128230f216f4038be7957
Aleksandar Babic <alekski64@icloud.com>
2018-08-10 09:50:37 +0200

    Added Gitlab parser

--- parsers/parser-factory.js
+++ parsers/parser-factory.js
@@ -1,10 +1,13 @@
 const GithubParser = require('./github');
+const GitlabParser = require('./gitlab');
 
 module.exports = {
   getParser: (url) => {
     switch (url) {
       case 'github.com':
         return new GithubParser();
+      case 'gitlab.com':
+        return new GitlabParser();
       default:
         throw new Error('Unknown parser requested.');
     }
